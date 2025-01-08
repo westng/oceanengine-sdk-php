389e5577659f587e186c158a2288eb36b8331368
west_ng <457395070@qq.com>
2025-01-08 14:00:48 +0800

    Add: Tests config

---
+++ tests/config.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Marketing PHP SDK.
+ *
+ * @link     https://github.com/westng/oceanengine-sdk-php
+ * @document https://github.com/westng/oceanengine-sdk-php
+ * @contact  westng
+ * @license  https://github.com/westng/oceanengine-sdk-php/blob/main/LICENSE
+ */
+const APPID = '';
+// Secret
+const SECRET = '';
+const AUTH_CODE = '';
+const TOKEN = '';
+const REFRESH_TOKEN = '';
+const ADVERTISER_ID = null;
+const CALLBACK_URL = '';
