8af84ce1097c542c1f9098a85370f07531c16098
west_ng <457395070@qq.com>
2025-01-09 13:40:20 +0800

    Add: AssetMgmt API

---
+++ src/Api/JuLiangAds/AssetMgmt/ImageVideoMgmt/QianchuanVideoByAwemeGet.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Marketing PHP SDK.
+ *
+ * @link     https://github.com/westng/oceanengine-sdk-php
+ * @document https://github.com/westng/oceanengine-sdk-php
+ * @contact  westng
+ * @license  https://github.com/westng/oceanengine-sdk-php/blob/main/LICENSE
+ */
+
+namespace Api\JuLiangAds\AssetMgmt\ImageVideoMgmt;
+
+use Core\Exception\InvalidParamException;
+use Core\Helper\RequestCheckUtil;
+use Core\Profile\RpcRequest;
+
+/**
+ * Name 获取抖音主页视频对应素材库视频
+ * Class QianchuanVideoByAwemeGet.
+ */
+class QianchuanVideoByAwemeGet extends RpcRequest
+{
+    protected string $url = '/v1.0/qianchuan/video/by_aweme/get/';
+
+    protected string $method = 'GET';
+
+    protected string $content_type = 'application/json';
+
+    /**
+     * 操作的广告主id.
+     */
+    protected int $advertiser_id;
+
+    /**
+     * @return $this
+     */
+    public function setArgs(mixed $args): static
+    {
+        foreach ($args as $key => $value) {
+            $this->params[$key] = $this->{$key} = $value;
+        }
+        return $this;
+    }
+
+    /**
+     * @throws InvalidParamException
+     */
+    public function check(): void
+    {
+        RequestCheckUtil::checkNotNull($this->advertiser_id, 'advertiser_id');
+    }
+}
