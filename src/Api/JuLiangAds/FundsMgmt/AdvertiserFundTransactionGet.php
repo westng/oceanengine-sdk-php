eedecb41578e07b1b55424512d7dc1043feb0649
west_ng <457395070@qq.com>
2025-01-09 10:22:22 +0800

    Add: Fund Management API

---
+++ src/Api/JuLiangAds/FundsMgmt/AdvertiserFundTransactionGet.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Marketing PHP SDK.
+ *
+ * @link     https://github.com/westng/oceanengine-sdk-php
+ * @document https://github.com/westng/oceanengine-sdk-php
+ * @contact  westng
+ * @license  https://github.com/westng/oceanengine-sdk-php/blob/main/LICENSE
+ */
+
+namespace Api\JuLiangAds\FundsMgmt;
+
+use core\Exception\InvalidParamException;
+use core\Helper\RequestCheckUtil;
+use core\Profile\RpcRequest;
+
+/**
+ * Name 查询财务流水明细
+ * Class AdvertiserFundTransactionGet
+ */
+class AdvertiserFundTransactionGet extends RpcRequest
+{
+    protected string $url = '/v1.0/qianchuan/finance/detail/get/';
+
+    protected string $method = 'GET';
+
+    protected string $content_type = 'application/json';
+
+    /**
+     * 操作的广告主id.
+     */
+    protected int $advertiser_id;
+
+    /**
+     * @return $this
+     */
+    public function setArgs(mixed $args): static
+    {
+        foreach ($args as $key => $value) {
+            $this->params[$key] = $this->{$key} = $value;
+        }
+        return $this;
+    }
+
+    /**
+     * @throws InvalidParamException
+     */
+    public function check(): void
+    {
+        RequestCheckUtil::checkNotNull($this->advertiser_id, 'advertiser_id');
+    }
+}
