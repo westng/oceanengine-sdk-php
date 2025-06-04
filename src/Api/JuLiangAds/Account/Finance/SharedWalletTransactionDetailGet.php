fb88699146c163b4491e719c5b2d72dc9e3a6c25
west_ng <457395070@qq.com>
2025-06-04 10:26:27 +0800

    feat(JuLiangAds/Finance): 增加 巨量广告/资金和流水管理 API

---
+++ src/Api/JuLiangAds/Account/Finance/SharedWalletTransactionDetailGet.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Marketing PHP SDK.
+ *
+ * @link     https://github.com/westng/oceanengine-sdk-php
+ * @document https://github.com/westng/oceanengine-sdk-php
+ * @contact  westng
+ * @license  https://github.com/westng/oceanengine-sdk-php/blob/main/LICENSE
+ */
+
+namespace Api\JuLiangAds\Account\Finance;
+
+use Core\Profile\RpcRequest;
+
+/**
+ * Name 资金共享-查询共享钱包流水明细
+ * Class SharedWalletTransactionDetailGet.
+ */
+class SharedWalletTransactionDetailGet extends RpcRequest
+{
+    protected string $url = '/v3.0/shared_wallet/transaction_detail/get/';
+
+    protected string $method = 'GET';
+
+    protected string $content_type = 'application/json';
+
+    /**
+     * @return $this
+     */
+    public function setArgs(mixed $args): static
+    {
+        foreach ($args as $key => $value) {
+            $this->params[$key] = $this->{$key} = $value;
+        }
+        return $this;
+    }
+}
---
+++ src/Api/JuLiangAds/Account/Finance/SharedWalletWalletRelationGet.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Marketing PHP SDK.
+ *
+ * @link     https://github.com/westng/oceanengine-sdk-php
+ * @document https://github.com/westng/oceanengine-sdk-php
+ * @contact  westng
+ * @license  https://github.com/westng/oceanengine-sdk-php/blob/main/LICENSE
+ */
+
+namespace Api\JuLiangAds\Account\Finance;
+
+use Core\Profile\RpcRequest;
+
+/**
+ * Name 资金共享-查询子钱包下绑定的adv列表
+ * Class SharedWalletWalletRelationGet.
+ */
+class SharedWalletWalletRelationGet extends RpcRequest
+{
+    protected string $url = '/v3.0/shared_wallet/wallet_relation/get/';
+
+    protected string $method = 'GET';
+
+    protected string $content_type = 'application/json';
+
+    /**
+     * @return $this
+     */
+    public function setArgs(mixed $args): static
+    {
+        foreach ($args as $key => $value) {
+            $this->params[$key] = $this->{$key} = $value;
+        }
+        return $this;
+    }
+}
