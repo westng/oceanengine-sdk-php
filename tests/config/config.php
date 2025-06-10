eb5da06435f2f2c9dbfa4d1c2f78b43efc5edf72
west_ng <457395070@qq.com>
2025-06-10 14:05:41 +0800

    fix(tests): 更新tests调用案例

--- tests/config/config.php
+++ tests/config/config.php
@@ -9,11 +9,11 @@ declare(strict_types=1);
  * @contact  westng
  * @license  https://github.com/westng/oceanengine-sdk-php/blob/main/LICENSE
  */
-const APPID = '';
-const SECRET = '';
-const AUTH_CODE = '';
-const TOKEN = '';
-const REFRESH_TOKEN = '';
-const ADVERTISER_ID = null;
-const ADVERTISER_IDS = [];
-const CALLBACK_URL = '';
+const APPID = '1784613942414368';
+const SECRET = '7578eb5922d27b5e9ad5cacbd0175395dd1509b7';
+const AUTH_CODE = 'WEST';
+const TOKEN = '135f3edf9be54a5ae0c2a225bab71b81c6abcb80';
+const REFRESH_TOKEN = 'a4a5d9bbc68047d4737af66b2e298cdc7515c2b3';
+const ADVERTISER_ID = 1805994941666500;
+const ADVERTISER_IDS = [1805994941666500];
+const CALLBACK_URL = 'https://back.recyou.cn/api/massive/callback/index';
